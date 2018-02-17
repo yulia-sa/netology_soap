6ccd40fbf7a7b444b668e71042741652f9510c3a
yulias <yulias@undev.ru>
2018-02-18 01:51:06 +0300

    problem-2 solved

--- sum_in_rubles.py
+++ sum_in_rubles.py
@@ -11,10 +11,41 @@ MOSCOW-LONDON: 120 EUR
 import os
 import osa
 import re
+import math
 
 
-currencies = "currencies"
+files = "files"
 current_dir = os.path.dirname(os.path.abspath(__file__))
-files_dir = os.path.join(current_dir, currencies)
-path_to_file_currencies = os.path.join(files_dir, "currencies.txt")
+path_to_file_currencies = os.path.join(current_dir, files, "currencies.txt")
 
+
+client = osa.Client('http://fx.currencysystem.com/webservices/CurrencyServer4.asmx?WSDL')
+
+
+def convert_currency_to_rub(licenseKey, fromCurrency, toCurrency, amount, rounding, date, type):
+    return client.service.ConvertToNum(licenseKey, fromCurrency, toCurrency, amount, rounding, date, type)
+
+
+def get_full_travel_cost_in_rub(path_to_file_currencies):
+    with open(path_to_file_currencies, encoding="utf-8") as f:
+        text = f.read()
+        costs_in_rub = []
+        costs_in_currency = re.findall('\d+\s\D{3}', text)
+
+        for x in costs_in_currency:
+            fromCurrency = re.findall('[A-Za-z]{3}', x)
+            amount = re.findall('\d+', x)
+
+            x_to_rub = convert_currency_to_rub(licenseKey='', fromCurrency=fromCurrency, toCurrency='RUB', 
+                                                amount=amount, rounding='false', date='', type='')
+            costs_in_rub.append(math.ceil(x_to_rub))
+
+        full_travel_cost_in_rub = sum(costs_in_rub)
+
+
+    return print("Full travel cost: " + str(full_travel_cost_in_rub) + " RUB")
+
+
+get_full_travel_cost_in_rub(path_to_file_currencies)
+
+  
