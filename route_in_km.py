fa4e9ccd7559779442558f3f6b966f4c2290be71
yulias <yulias@undev.ru>
2018-02-18 03:16:23 +0300

    problem-3 solved

--- route_in_km.py
+++ route_in_km.py
@@ -7,3 +7,39 @@
 MOSCOW-LONDON: 1,553.86 mi
 Необходимо посчитать суммарное расстояние пути в километрах с точностью до сотых.
 '''
+
+import os
+import osa
+import re
+import math
+
+
+files = "files"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path_to_file_travel = os.path.join(current_dir, files, "travel.txt")
+
+
+client = osa.Client('http://www.webservicex.net/length.asmx?WSDL')
+
+
+def convert_miles_to_km(LengthValue, fromLengthUnit, toLengthUnit):
+    return client.service.ChangeLengthUnit(LengthValue, fromLengthUnit, toLengthUnit)
+
+def get_full_route_in_km(path_to_file_travel):
+    with open(path_to_file_travel, encoding="utf-8") as f:
+        text = f.read()
+        parts_of_route_in_km = []
+        raw_parts_of_route_in_miles = re.findall('[0-9]{1,}\,{0,1}[0-9]{0,}\.{0,1}[0-9]{0,}', text)
+
+        for raw_part in raw_parts_of_route_in_miles:
+            part = raw_part.replace(',', '')
+            length_value = float(part)
+            part_in_km = convert_miles_to_km(LengthValue=length_value, fromLengthUnit='Miles', toLengthUnit='Kilometers')
+            parts_of_route_in_km.append(part_in_km)
+
+        full_route_in_km = round(sum(parts_of_route_in_km), 2)
+
+    return print("Full route: " + str(full_route_in_km) + " km")
+
+
+get_full_route_in_km(path_to_file_travel)
